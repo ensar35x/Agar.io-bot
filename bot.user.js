8d21dc652dc209bf680dc3d38f2fbbcd5da207ff
ensar35x <ensar35x@gmail.com>
2015-07-22 22:15:34 +0300

    bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        AposBot
 // @namespace   AposBot
-// @include     http://agar.io/*
+// @include     http://agaryator.com/*
 // @version     3.551
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
